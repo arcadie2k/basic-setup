e243657cdfc244838a422473e4101987fe81d842
Caldare Arcadie <arcadie2klearn@gmail.com>
2020-09-16 17:07:25 +0300

    just one epxress endpoint

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const PORT = process.env.NODE_ENV || 8080
+const app = express()
+
+app.get('/', (req, res) => {
+  console.log('GOT IT!')
+  res.send('App working just fine!')
+})
+
+app.listen(PORT, () => console.log(`APP RUNNING ON ** http://localhost:${ PORT } **`))
