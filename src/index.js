af2259dece2c5c3abc5c52df495cd5ef24a08214
Caldare Arcadie <arcadie2klearn@gmail.com>
2020-09-16 17:17:20 +0300

    added middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 
-const PORT = process.env.PORT || 8080
-const app = express()
+const init = async () => {
+  const PORT = process.env.PORT || 8080
+  const app = express()
+  
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-  console.log('GOT IT!')
-  res.send('App working just fine!')
-})
+  app.get('/', (req, res) => {
+    console.log('GOT IT!')
+    res.send('App working just fine!')
+  })
+  
+  app.listen(PORT, () => console.log(`APP RUNNING ON ** http://localhost:${ PORT } **`))
+}
 
-app.listen(PORT, () => console.log(`APP RUNNING ON ** http://localhost:${ PORT } **`))
+init()
